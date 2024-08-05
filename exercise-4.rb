fc36976e8060fc352f96d7233c79bf4070cc7c84
Dao Thanh Tri <119103571+daothanhtri@users.noreply.github.com>
2024-08-05 22:24:26 +0700

    Update exercise-4.rb

--- exercise-4.rb
+++ exercise-4.rb
@@ -1,20 +1,25 @@
 
 array = Array.new
-n = 0
+i = 0
 number_exist = false
 
-until n > 99 do
-  array[n] = (rand * 100).round(0)
-  n = n + 1
+puts "Please input size of number's list: "
+n = gets.chomp.to_i 
+
+until i >= n do
+  array[i] = (rand * 100).round(0)
+  i = i + 1
 end
 
+puts "===================================="
+puts "your array: "
 p array
 
 puts "Please input a number: "
-x = gets.chomp.to_i 
+x = gets.chomp
 
 array.each do |num| 
-  number_exist = true if num == x
+  number_exist = true if num == x.to_i
 end
 
 if number_exist == false

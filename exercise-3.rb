0c4a693cb51e94bd45bb38eb538c49be2e97410f
Dao Thanh Tri <119103571+daothanhtri@users.noreply.github.com>
2024-08-05 22:23:59 +0700

    Update exercise-3.rb

--- exercise-3.rb
+++ exercise-3.rb
@@ -1,20 +1,25 @@
 
 array = Array.new
-n = 0
+i = 0
 count = 0
 
-until n > 99 do
-  array[n] = (rand * 100).round(0)
-  n = n + 1
+puts "Please input size of number's list: "
+n = gets.chomp.to_i 
+
+until i >= n do
+  array[i] = (rand * 100).round(0)
+  i = i + 1
 end
 
+puts "===================================="
+puts "your array: "
 p array
 
 puts "Please input a number: "
-x = gets.chomp.to_i 
+x = gets.chomp
 
 array.each do |num| 
-  count = count + 1 if num == x
+  count = count + 1 if num == x.to_i
 end
 
 puts "count of number #{x} is: #{count}"
